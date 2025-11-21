6660315ddcea49ba842b01727fcf1b6dac68fb90
Tauqeer-flutter <tauqeer.flutter@tafsol.com>
2025-11-21 22:20:36 +0500

    feat: enable minify and proguard for release builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,14 +28,16 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        minifyEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
